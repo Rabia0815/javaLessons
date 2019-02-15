f843a56b7f5eafa7586c6453ef3370aaab628430
Rabia0815 <33806074+Rabia0815@users.noreply.github.com>
2019-02-15 12:04:56 +0200

    Add files via upload

---
+++ MyFirstProject/src/com/epam/lesson1/Lesson1.java
@@ -0,0 +1,11 @@
+package com.epam.lesson1;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("* * * *    * * * *        *       *      *");
+        System.out.println("*          *     *       * *      * *  * *");
+        System.out.println("* *        * * * *      *   *     *   *  *");
+        System.out.println("*          *           * * * *    *      *");
+        System.out.println("* * * *    *          *       *   *      *");
+    }
+}
